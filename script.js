d8a3145d766bc4d0c8ec424e7dbdd7059417ede5
Christian Mpabuka <christianmpabuka@gmail.com>
2023-09-30 00:41:21 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed= new typed(".text",{
+    Strings:["Fashion Stylist", "Style Consultant", "Content Creator"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+console.log(text);
