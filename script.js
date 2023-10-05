3a09921f7fc8ea70237120e4524ec062779763ff
Christian Mpabuka <christianmpabuka@gmail.com>
2023-10-04 22:27:42 -0500

    my services

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-var typed= new typed(".text",{
-    Strings:["Fashion Stylist", "Style Consultant", "Content Creator"],
-    typeSpeed:100,
-    backSpeed:100,
+var typed= new Typed('.text',{
+    strings:['Fashion Stylist', 'Style Consultant', 'Content Creator'],
+    typeSpeed:110,
+    backSpeed:110,
     backDelay:1000,
-    loop:true
+    loop:true,    
 });
-console.log(text);
